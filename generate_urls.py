f0f085dd0892f09c87a750f80cfa640cb73bdb2f
habib <has.sorathia@gmail.com>
2020-05-23 14:12:33 -0700

    updated for mol

--- generate_urls.py
+++ generate_urls.py
@@ -1,10 +1,8 @@
 # returns list_of_urls which is a list of urls. one url per chapter of mol
 def mol_links():
     list_of_urls = []
-    
     # url for the first chapter of mother of learning
     base_url = 'https://www.fictionpress.com/s/2961893/1/Mother-of-Learning'
-    
     # there are 108 chapters in mother of learning
     chapter_numbers = 108
     for i in range(1, chapter_numbers + 1):
--- html_to_latex.py
+++ html_to_latex.py
@@ -1,4 +1,5 @@
-import requests, os
+import requests
+import os
 from bs4 import BeautifulSoup
 from generate_urls import mol_links
 
@@ -13,17 +14,34 @@ def mol_html():
     if not os.path.exists(path):
         os.makedirs(path)
 
-    for i in range(len(urls)):
-        req = requests.get(urls[i], headers)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        html_input = soup.find_all('p')
-        with open(os.path.join(path, 'chapter_' + str(i + 1) + '.tex'), 'w+') as chapter:
-            chapter.write('\\documents{article}\n\\usepackage[utf8]{inputenc}\n\n')
-            chapter.write('\\title{Mother of Learning - Chapter ' + str(i + 1) +' }\n')
-            chapter.write('\\author{nobody103}\n\n\\begin{document}\n\\maketitle\n')
+    with open(os.path.join(path, 'MotherOfLearning.tex'), 'w+') as book:
+        book.write('\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n\n')
+        book.write('\\title{Mother of Learning}\n')
+        book.write('\\author{nobody103}\n\n\\begin{document}\n\\maketitle\n\n')
+        for i in range(len(urls)):
+            book.write('\n\\section{Chapter ' + str(i + 1) + '}\n\n\n')
+            req = requests.get(urls[i], headers)
+            soup = BeautifulSoup(req.content, 'html.parser')
+            html_input = soup.find_all('p')
+            # bolded_words = html_input.html.find_all('strong')
+            # if len(bolded_words) > 1:
+            #     for i in range(len(bolded_words)):
+            #         bolded_words[i].string = '\\textbf{' + bolded_words[i].text + '}'
+            # italic_words = html_input.html.find_all('em')
+
             for line in html_input:
                 # process each line here
-            chapter.write('\n\\end{document}')
+                # text = convert2latex(line)
+                book.write(line.text + '\n\n')
+                # book.write(line.text + '\n\n\n')
+        book.write('\n\\end{document}')
+
+
+# def convert2latex(element):
+    # if len(element) > 1:
+    #     convert2latex(element)
+    # print(element.tag)
+    # return element.text
 
 
 mol_html()
