3267fb7d7a3cb8705af9ea7578587521582a1f9f
habib <has.sorathia@gmail.com>
2020-05-31 21:19:24 -0700

    MOL completed

--- html_to_latex.py
+++ html_to_latex.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import string
 from bs4 import BeautifulSoup
 from generate_urls import mol_links
 
@@ -15,11 +16,24 @@ def mol_html():
         os.makedirs(path)
 
     with open(os.path.join(path, 'MotherOfLearning.tex'), 'w+') as book:
-        book.write('\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n\n')
+        book.write('\\documentclass[letterpaper, 12pt, twoside]{book}\n')
+        book.write('\\usepackage[english]{babel}\n')
+        book.write('\\usepackage[utf8]{inputenc}\n')
+        # book.write('\\usepackage{tabto}\n')
+        book.write('\\usepackage[nomarginpar, margin=1in]{geometry}\n')
+        book.write('\\usepackage{indentfirst}\n\n')
         book.write('\\title{Mother of Learning}\n')
-        book.write('\\author{nobody103}\n\n\\begin{document}\n\\maketitle\n\n')
+        book.write('\\author{nobody103}\n\n')
+        book.write('\\setlength{\\parindent}{2em}\n')
+        book.write('\\setlength{\\parskip}{.5em}\n')
+        book.write('\\renewcommand{\\baselinestretch}{1.6}\n\n')
+        book.write('\\begin{document}\n\n')
+        book.write('\\maketitle\n\n')
+
         for i in range(len(urls)):
-            book.write('\n\\section{Chapter ' + str(i + 1) + '}\n\n\n')
+            print('Converting Chapter ' + str(i + 1))
+            book.write('\\section*{\\centering Chapter ' + str(i + 1) + '}\n')
+            book.write('\\markboth{Chapter }{' + str(i + 1) + '}\n\n')
             req = requests.get(urls[i], headers)
             soup = BeautifulSoup(req.content, 'html.parser')
             html_input = soup.find_all('p')
@@ -28,20 +42,34 @@ def mol_html():
             #     for i in range(len(bolded_words)):
             #         bolded_words[i].string = '\\textbf{' + bolded_words[i].text + '}'
             # italic_words = html_input.html.find_all('em')
-
+           
             for line in html_input:
+                for i in range(1, len(line.text)):
+                    if i % 120 == 0:
+                        # this is just to make the tex file have less issues
+                        # with overflow as i manually enter a whitespace
+                        # this might be causing me problems where words have a
+                        # random space. to fix this, probably change the '\n'
+                        # to just \n
+                        book.write('\n')
+
+                    # current eror checkking only handles open and close quotes
+                    # this feature should later be expnanded to also handle
+                    # special expressions for latex bc characters like &
+                    # are reserved
+                    if line.text[i-1] == '\"':
+                        # print(line.text[i], line.text[i] == ' ')
+                        if line.text[i] == ' ':
+                            book.write('\"')
+                        else:
+                            book.write('``')
+                        continue
+                    book.write(line.text[i-1])
                 # process each line here
                 # text = convert2latex(line)
-                book.write(line.text + '\n\n')
+                book.write('\n\n')
                 # book.write(line.text + '\n\n\n')
-        book.write('\n\\end{document}')
-
-
-# def convert2latex(element):
-    # if len(element) > 1:
-    #     convert2latex(element)
-    # print(element.tag)
-    # return element.text
+        book.write('\\end{document}\n\n')
 
 
 mol_html()
